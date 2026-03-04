bc3b67027ebdeb43d6132d2fd93c34ca9fd6ce72
DonDCas <fjrueda92@gmail.com>
2026-03-04 20:10:29 +0000

    actualizacion server

--- backend/settings.py
+++ backend/settings.py
@@ -198,10 +198,13 @@ SPECTACULAR_SETTINGS = {
 }
 
 APPEND_SLASH = True
-CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-    "http://localhost:4200",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+#CORS_ALLOWED_ORIGINS = '*'
+
+#CORS_ALLOW_ALL_ORIGINS = False
+#CORS_ALLOWED_ORIGINS = [
+#    "http://127.0.0.1:5500",
+#    "http://localhost:5500",
+#    "http://localhost:4200",
+#]
 CORS_ALLOW_CREDENTIALS = True
